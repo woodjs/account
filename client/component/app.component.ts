6101b41b47205c3078a84211c3c96e181ce996d8
yinyanlv <yanlv.yin@gmail.com>
2016-10-21 17:57:05 +0800

    提交修改

--- client/component/app.component.ts
+++ client/component/app.component.ts
@@ -4,12 +4,7 @@ import {Component} from '@angular/core';
   moduleId: module.id,
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
+    <a href="#">abc</a>
   `,
   styleUrls: ['../style/app.component.css']
 })
--- client/module/app.module.ts
+++ client/module/app.module.ts
@@ -1,37 +1,17 @@
-import '../config/rxjs-extensions';
-
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'
-import {HttpModule} from '@angular/http';
-import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
 
-import {appRoute} from '../route/app.route';
 import {AppComponent} from '../component/app.component';
-import {DashboardComponent} from "../component/dashboard.component";
-import {HeroSearchComponent} from '../component/hero-search.component';
-import {HeroesComponent} from '../component/heroes.component';
-import {HeroDetailComponent} from '../component/hero-detail.component';
-import {HeroService} from '../service/hero.service';
-import {HeroesInMemory} from '../mock/mock-heroes';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(HeroesInMemory),
-    appRoute
   ],
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    HeroSearchComponent
+    AppComponent
   ],
   providers: [
-    HeroService
   ],
   bootstrap: [
     AppComponent
